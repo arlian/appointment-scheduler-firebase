bcbe538028ac7b8f81ff491868356f8c1b09a695
arlian <gutamaarlian@gmail.com>
2026-09-22 11:22:54 +0700

    update versi sw

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker: utamakan jaringan, cadangkan cache agar tetap bisa dibuka offline.
-const CACHE = 'jadwal-treatment-v53';
+const CACHE = 'jadwal-treatment-v54';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './firebase-config.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
